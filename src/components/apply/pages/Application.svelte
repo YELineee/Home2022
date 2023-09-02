9680d625b42d4e9fd5542ac438292e8873be3b33
YEL!ne <1638317920@qq.com>
2023-09-02 16:55:53 +0800

    Change the QRCord for interview

--- src/components/apply/pages/Application.svelte
+++ src/components/apply/pages/Application.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <div transition:fly class="flex flex-col justify-center items-center gap-5">
-	<a href="https://docs.qq.com/form/page/DZkhBWWdObHZYak1U">
+	<p>点击或扫码开始填写</p>
+	<a href="https://poqs387trw.feishu.cn/share/base/form/shrcnKgsBiIwhx8R9xU3L4DwZhe">
 		<img
 			class="w-96 hover:shadow-lg rounded-lg transition-shadow duration-300"
 			src="apply/QRCode.png"
-			alt="qr code"
+			alt="fs code"
 		/>
 	</a>
-	<p>点击或扫码开始填写</p>
 </div>
