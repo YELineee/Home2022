e4bedaf05d6b56a81ae72e52c44b162aa4806464
YEL!ne <1638317920@qq.com>
2023-09-02 22:06:58 +0800

    Fix double click

--- src/components/menu/Item.svelte
+++ src/components/menu/Item.svelte
@@ -1,11 +1,14 @@
 <script>
-import { ChevronRightIcon } from '@rgossiaux/svelte-heroicons/outline';
-
+	import { ChevronRightIcon } from '@rgossiaux/svelte-heroicons/outline';
 	import { slide } from 'svelte/transition';
 	export let href = '/';
-</script>
-
-<a transition:slide {href} class="w-full p-2 flex justify-center items-center group">
-	<ChevronRightIcon class="w-6 h-6 group opacity-0 group-hover:opacity-100 transition-opacity duration-300"/>
+	let isClicked = false;
+	const handleClick = () => {
+	  isClicked = true;
+	};
+  </script>
+  
+  <a on:click={handleClick} transition:slide {href} class="w-full p-2 flex justify-center items-center group">
+	<ChevronRightIcon class="w-6 h-6 group {isClicked ? 'opacity-100' : 'opacity-0'} transition-opacity duration-300"/>
 	<slot />
-</a>
+  </a>
